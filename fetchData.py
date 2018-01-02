566909e0ccbacc9b71ea25ed7fc1c7e40e50e1cc
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2018-01-02 18:56:13 +0000

    Add fetchCommitSearchData for github api

---
+++ fetchData.py
@@ -0,0 +1,38 @@
+import requests
+import datetime
+import json
+
+def saveJSON(filename, data):
+    with open (filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+def fetch(url, params, custom_headers, returnFunc):
+    params["client_id"] = 'ad8123c5877d0fb7ae5a'
+    params["client_secret"] = '13d3a67e4c92de09ae5f7553c5acdfd0783eddc3'
+    res = requests.get(url, params, headers=custom_headers)
+    print(res.url)
+    if (res.status_code != 200):
+        try:
+            res.raise_for_status()
+        except requests.HTTPError as ex:
+            print(ex)
+            resetTime = res.headers['X-RateLimit-Reset']
+            if resetTime != None:
+                print(f"X-RateLimit-Remaining: {req.headers['X-RateLimit-Remaining']}")
+                print(f"X-RatLimit-Reset: {datetime.datetime.fromtimestamp(int(resetTime))}")
+                print(res.headers)
+            raise ex
+    return returnFunc(res)
+
+def fetchJSON(url, params, custom_headers):
+    return fetch(url, params, custom_headers, lambda res: res.json())
+
+def fetchCommitSearchData(date, page, fetchFunc=fetchJSON):
+    url = "https://api.github.com/search/commits"
+    params = {"q": "committer-date:" + str(date), "page":str(page)}
+    custom_headers = {"Accept":"application/vnd.github.cloak-preview"}
+    return fetchFunc(url, params, custom_headers)
+
+result = fetchCommitSearchData(datetime.date(2017,12,1), 1)
+
+saveJSON("test.json", result)
